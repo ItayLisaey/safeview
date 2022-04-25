edfd5da63aced084e1cc60e5d60e09974797d6d1
Itay Lisaey <et030102@gmail.com>
2022-04-25 18:45:40 +0300

    style: changed preview to match example

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import SafeView from './lib';
 function App() {
   return (
     <div className={classes.container}>
-      <SafeView accessKey='so'>
+      <SafeView accessKey='so' filter='blur'>
         <div className={classes.box}>
-          <h1 data-safe>Hello World</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio
+            laborum illo recusandae ullam, quas quos! Aliquid a, nihil tempore
+            accusamus facere hic earum assumenda soluta perferendis itaque
+            explicabo reprehenderit. Nostrum, aperiam voluptatem optio saepe
+            recusandae veritatis reprehenderit error quo commodi eligendi dolore
+            nemo eum ullam? Culpa odit reprehenderit dolore labore ab molestiae
+            nam fugit tempora, quo,
+            <strong data-safe>
+              deserunt amet repudiandae saepe doloremque dignissimos praesentium
+              quidem nulla!
+            </strong>
+            Quasi aliquam soluta delectus pariatur, sint, sequi maxime error
+            magni, inventore animi beatae architecto aspernatur? Enim quis nemo
+            tempore qui, rem, ducimus, magni deleniti illo odio consectetur
+            eveniet optio incidunt. Facere natus commodi soluta amet?
+          </p>
         </div>
       </SafeView>
     </div>
